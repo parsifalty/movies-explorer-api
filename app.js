27533a674f531a3df16afd941a403454c3688ce7
Pasha Litvak <parsifalty@gmail.com>
2024-02-15 02:09:07 +0200

    sdjdsids

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const cors = require("cors");
 
-const { PORT = 3000, DB_URL = "mongodb://0.0.0.0:27017/diploma" } = process.env;
+const { PORT = 3001, DB_URL = "mongodb://0.0.0.0:27017/diploma" } = process.env;
 
 const app = express();
 
